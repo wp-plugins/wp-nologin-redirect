9fd554e422dcfb42d6eace07985d51609df9097e
yat8823jp <yat8823jp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 11:34:11 +0000

    Change init at the timing of the redirect
    git-svn-id: https://plugins.svn.wordpress.org/wp-nologin-redirect/tags/1.1.3@999067 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nologin-redirect.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package no-login-redirect
+ * @version 1.0
+**/
+/*
+Plugin Name:no-login-redirect
+Plugin URI:
+Description:非ログイン時、ログイン画面にリダイレクトさせる
+Author YAT,Chiaki
+Version:1.0
+*/
+function nlr_theme_name_script(){
+	wp_enqueue_style( 'nologinredirect' , plugins_url('nologin-redirect-style.css', __FILE__ ) );
+}
+add_action( 'wp_enqueue_scripts', 'nlr_theme_name_script' );
+
+function nlr_no_login_redirect($content){
+	global $pagenow;
+	if( !is_user_logged_in() && !is_admin() && ( $pagenow != 'wp-login.php' ) ){
+		$url = ( empty( $_SERVER["HTTPS"] ) ? 'http://' : 'https://' ) . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+		wp_safe_redirect( wp_login_url( $url ) );
+		exit;
+	}
+}
+
+add_action( 'init', 'nlr_no_login_redirect' );
+?>
