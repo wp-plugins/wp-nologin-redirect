dfe9c7bbf154c092de8e1fee01963b7f5ed92bbc
yat8823jp <yat8823jp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 07:55:48 +0000

    tag-up
    git-svn-id: https://plugins.svn.wordpress.org/wp-nologin-redirect/tags/1.1@998963 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nologin-redirect.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package no-login-redirect
+ * @version 1.0
+**/
+/*
+Plugin Name:no-login-redirect
+Plugin URI:
+Description:非ログイン時、ログイン画面にリダイレクトさせる
+Author YAT,Chiaki
+Version:1.0
+*/
+function nlr_theme_name_script(){
+	wp_enqueue_style( 'nologinredirect' , plugins_url('nologin-redirect-style.css', __FILE__ ) );
+}
+add_action( 'wp_enqueue_scripts' , 'nlr_theme_name_script');
+
+function nlr_no_login_redirect($content){
+	if( !is_user_logged_in() ){
+		$url = ( empty( $_SERVER["HTTPS"] ) ? 'http://' : 'https://' ) . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+		wp_safe_redirect( wp_login_url( $url ) );
+	}
+}
+
+add_action('wp_head', 'nlr_no_login_redirect');
+?>
